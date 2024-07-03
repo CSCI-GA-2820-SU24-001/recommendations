6860419de356a639a5a863f9ef9911a2cb458208
Rebeccaxxxxx <xw1706@nyu.edu>
2024-07-03 22:26:23 +0800

    Bug fix
    Change from "pet store" to our "recommendation", remove extra lines, change model in factory

--- tests/factories.py
+++ tests/factories.py
@@ -3,11 +3,12 @@ Test Factory to make fake objects for testing
 """
 
 import factory
+from factory.fuzzy import FuzzyChoice
 from service.models import Recommendation
 
 
 class RecommendationFactory(factory.Factory):
-    """Creates fake pets that you don't have to feed"""
+    """Creates fake recommendations"""
 
     class Meta:  # pylint: disable=too-few-public-methods
         """Maps factory to data model"""
@@ -15,6 +16,7 @@ class RecommendationFactory(factory.Factory):
         model = Recommendation
 
     id = factory.Sequence(lambda n: n)
-    name = factory.Faker("first_name")
-
-    # Todo: Add your other attributes here...
+    name = factory.Faker("name")
+    product_id = factory.Faker("random_int", min=1, max=1000)
+    recommended_product_id = factory.Faker("random_int", min=1, max=1000)
+    recommendation_type = FuzzyChoice(["cross-sell", "up-sell", "accessory"])
